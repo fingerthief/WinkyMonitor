d6ebd4f575cbd2ff0c25a71f6caf54386c3f4a42
Tanner Middleton <middletontanner@gmail.com>
2013-01-12 16:29:23 -0600

    Added License

--- ConkyForWindows/Options.xaml.cs
+++ ConkyForWindows/Options.xaml.cs
@@ -13,6 +13,29 @@ namespace Winky
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
+    /// 
+    /// <remarks>
+    /// WinkyMonitor is a system monitor. Written by Tanner L. Middleton and Greg Morgan
+    /// Copyright (C) 2013
+    /// 
+    /// This program is free software; you can redistribute it and/or
+    /// modify it under the terms of the GNU General Public License
+    /// as published by the Free Software Foundation; either version 2
+    /// of the License, or (at your option) any later version.
+    /// 
+    /// This program is distributed in the hope that it will be useful,
+    /// but WITHOUT ANY WARRANTY; without even the implied warranty of
+    /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    /// GNU General Public License for more details.
+    /// 
+    /// You should have received a copy of the GNU General Public License
+    /// along with this program; if not, write to the Free Software
+    /// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+    /// 
+    /// The author may be contacted at:
+    /// middletontanner@gmail.com or gamm90@gmail.com
+    /// </remarks>
+    
     public partial class Window1 : Window
     {
         private MainWindow mainWindow;
