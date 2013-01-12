f5adef02c83a4c21cccfe6e94390c20206e3edcc
Tanner Middleton <middletontanner@gmail.com>
2013-01-12 16:31:12 -0600

    Fixed License

--- ConkyForWindows/MainWindow.xaml.cs
+++ ConkyForWindows/MainWindow.xaml.cs
@@ -39,10 +39,6 @@ namespace Winky
     /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     /// GNU General Public License for more details.
     /// 
-    /// You should have received a copy of the GNU General Public License
-    /// along with this program; if not, write to the Free Software
-    /// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-    /// 
     /// The author may be contacted at:
     /// middletontanner@gmail.com or gamm90@gmail.com
     /// </remarks>
